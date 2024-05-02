87e702c99983d16b208d8af8c61b084be9f4afd3
iDevArya <aryashiva1126@gmail.com>
2024-05-02 19:11:03 -0400

    Initial Commit

---
+++ Project Tracker/Project_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_TrackerApp.swift
+//  Project Tracker
+//
+//  Created by Shiva kumar Arya on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Project_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
