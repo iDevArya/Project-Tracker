f6588fab8f3c5acc78f2f7bce9b4e22eac86a506
iDevArya <aryashiva1126@gmail.com>
2024-05-02 20:24:45 -0400

    - Add Image Assets - Add Color Assets - Configure fonts in FontHelper

---
+++ Project Tracker/Helpers/FontHelper.swift
@@ -0,0 +1,19 @@
+//
+//  FontHelper.swift
+//  Project Tracker
+//
+//  Created by Shiva kumar Arya on 5/2/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    static var screenHeading = Font.system(size: 36, weight: .bold)
+    static var bigHeadline = Font.system(size: 15, weight: .bold)
+    static var smallHeadline = Font.system(size: 10, weight: .bold)
+    static var featuredNumber = Font.system(size: 18)
+    static var featuredText = Font.system(size: 12)
+    static var regularText = Font.system(size: 10)
+    static var captionText = Font.system(size: 8)
+}
